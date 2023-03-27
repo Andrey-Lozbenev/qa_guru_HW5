d79faf1cc2e5b9c68afeb763a0eca10cb1048c6f
Andrey <hanom_dzr@mail.ru>
2023-03-27 13:47:07 +0300

    first commit

---
+++ src/test/java/TestHover.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestHover {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void testGithubHover() {
+        open("https://github.com");
+        $(".header-menu-wrapper").$(byText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(".enterprise-hero").shouldHave(text("Build like the best"));
+    }
+}
+
+
